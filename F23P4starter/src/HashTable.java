68b6406808dbd0a41d85c5a9a030cc2434198005
mohammadm147 <129554734+mohammadm147@users.noreply.github.com>
2023-11-07 14:15:57 -0500

    Update HashTable.java

--- F23P4starter/src/HashTable.java
+++ F23P4starter/src/HashTable.java
@@ -8,7 +8,7 @@ public class HashTable {
     public HashTable(int hashSize) {
         this.hashSize = hashSize;
         table = new Record[hashSize];
-        recordCount = 0;z
+        recordCount = 0;
     }
 
 
@@ -28,20 +28,68 @@ public class HashTable {
 
 
     public void insert(Record record) {
-        if (recordCount >= (hashSize / 2)) {
+        if (seminarCount >= (hashSize / 2)) {
             rehash();
-            System.out.println()
+            System.out.println("Hash table expanded to " + hashSize
+                + " records");
+        }
+        record.setId(id);
+        int key = keyFinder1(id);
+        while ((hashTable[key] != null) && (hashTable[key].getId() != -1)) {
+            key = keyFinder2(id, key);
+        }
+        record.setKey(key);
+        hashTable[record.getKey()] = record;
+        seminarCount++;
+        }
+
+
+    public void delete(String title) {
+        int found = this.search(id);
+
+        if (found != -1) {
+            Record temp = new Record();
+            temp.setId(-1);
+            temp.setKey(found);
+            hashTable[found] = temp;
+            System.out.println("Record with ID " + id
+                + " successfully deleted from the database");
+            seminarCount--;
+        }
+        else {
+            System.out.println("Delete FAILED -- There is no record with ID "
+                + id);
         }
     }
 
 
-    public void delete(String title) {
+    public void search(String title) {
+        int found = -1;
+        int key = keyFinder1(id);
+        while (hashTable[key] != null) {
+            if (hashTable[key].getId() == id) {
+                found = key;
+                return found;
+            }
+            else {
+                key = keyFinder2(id, key);
+            }
+        }
+        return found;
 
     }
+    
+    private void rehash() {
+        hashSize = hashSize * 2;
+        HashTable updatedHashTable = new HashTable(hashSize);
 
-
-    public void search(String title) {
-
+        for (int i = 0; i < hashTable.length; i++) {
+            if ((hashTable[i] != null) && (hashTable[i].getId() != -1)) {
+                int tempId = hashTable[i].getId();
+                updatedHashTable.insert(tempId, hashTable[i]);
+            }
+        }
+        hashTable = updatedHashTable.getArr();
     }
 
 }
